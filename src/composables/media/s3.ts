3e12da31c3378fb662f835653bed44995a12506c
Chendada-8474 <raymond96383@gmail.com>
2025-04-20 22:01:10 +0800

    feat: image for dev

--- src/composables/media/s3.ts
+++ src/composables/media/s3.ts
@@ -2,10 +2,14 @@ import { useEnv } from '@/composables/useEnv'
 
 const env = useEnv()
 
+// This is a devolepment function for environment without S3 service.
 export const convertIDToS3Link = (id: string, isImage: boolean): string => {
-  if (isImage) {
-    return `${env}${id}.jpg`
-  } else {
-    return `${env}${id}.mp4`
-  }
+  return new URL('@/assets/demo_media.jpg', import.meta.url).href
 }
+// export const convertIDToS3Link = (id: string, isImage: boolean): string => {
+//   if (isImage) {
+//     return `${env}${id}.jpg`
+//   } else {
+//     return `${env}${id}.mp4`
+//   }
+// }
