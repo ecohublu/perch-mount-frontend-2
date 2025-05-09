bef7d76e6157586130ad35f2961ba981351a0d4d
Chendada-8474 <raymond96383@gmail.com>
2025-05-09 18:53:12 +0800

    feat: add section in admin

--- src/components/adminviews/PerchmountView.vue
+++ src/components/adminviews/PerchmountView.vue
@@ -5,14 +5,31 @@
     </SplitterPanel>
     <SplitterPanel class="flex justify-center p-8" :size="65">
       <div>
-        <Button label="新增 Section" icon="pi pi-plus" rounded text />
+        <Button
+          label="新增 Section"
+          icon="pi pi-plus"
+          rounded
+          text
+          @click="AddSectionVisible = !AddSectionVisible"
+        />
         <SectionsOfPerchMountTable :perchMountId="id"></SectionsOfPerchMountTable>
       </div>
     </SplitterPanel>
   </Splitter>
+  <Dialog
+    v-model:visible="AddSectionVisible"
+    modal
+    header="新增 Section"
+    :style="{ width: '30rem' }"
+  >
+    <AddSectionForm :perch-mount-id="id"></AddSectionForm>
+  </Dialog>
 </template>
 <script setup lang="ts">
 import PerchMountEditor from '@/components/adminviews/editors/PerchMountEditor.vue'
 import SectionsOfPerchMountTable from '@/components/adminviews/tables/SectionsOfPerchMountTable.vue'
+import AddSectionForm from '@/components/forms/AddSectionForm.vue'
+import { ref } from 'vue'
 const props = defineProps<{ id: string }>()
+const AddSectionVisible = ref<boolean>(false)
 </script>
