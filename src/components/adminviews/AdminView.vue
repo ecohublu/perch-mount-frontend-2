b021ceb29dd026bc9134d7d8f0d056e7fa8c8992
Chendada-8474 <raymond96383@gmail.com>
2025-05-10 11:33:55 +0800

    feat: move add project button to admin area

--- src/components/adminviews/AdminView.vue
+++ src/components/adminviews/AdminView.vue
@@ -1,17 +1,40 @@
 <template>
-  <Loading v-if="isLoading"></Loading>
+  <Loading v-if="useTreeNodesLoading"></Loading>
   <Tree v-else :value="treeNodes" :filter="true" filterMode="lenient" class="w-full md:w-[30rem]">
     <template #default="slotProps">
       <router-link :to="slotProps.node.data.to">{{ slotProps.node.label }}</router-link>
     </template>
   </Tree>
+  <Button label="新增計畫" icon="pi pi-plus" class="mt-6" @click="addProjectVisiable = true" />
+  <Dialog v-model:visible="addProjectVisiable" modal header="新增計畫" :style="{ width: '25rem' }">
+    <AddProjectForm @added="handleProjectAdded"></AddProjectForm>
+  </Dialog>
 </template>
 <script setup lang="ts">
 import { useTreeNodes } from '@/composables/useProjectsPerchMountsTreeNodes'
-import { onMounted } from 'vue'
-
+import { onMounted, ref } from 'vue'
 import Loading from '@/components/Loading.vue'
+import { useProjects } from '@/composables/projects/useProjects'
 
-const { treeNodes, isLoading, error, fetch } = useTreeNodes()
+const addProjectVisiable = ref<boolean>(false)
+
+const {
+  treeNodes,
+  isLoading: useTreeNodesLoading,
+  error: treeNodesError,
+  fetch: fetchTreeNodes,
+} = useTreeNodes()
+onMounted(fetchTreeNodes)
+
+const {
+  data: projects,
+  isLoading: usProjectsLoading,
+  error: projectsError,
+  fetch: fetchProjects,
+} = useProjects()
 onMounted(fetch)
+
+const handleProjectAdded = () => {
+  fetchProjects()
+}
 </script>
--- src/pages/app/projects/index.vue
+++ src/pages/app/projects/index.vue
@@ -2,10 +2,6 @@
   <div class="grid grid-cols-2 gap-4">
     <ProjectCard v-for="project in projects" :info="project"></ProjectCard>
   </div>
-  <Button label="新增計畫" icon="pi pi-plus" class="mt-6" @click="addProjectVisiable = true" />
-  <Dialog v-model:visible="addProjectVisiable" modal header="新增計畫" :style="{ width: '25rem' }">
-    <AddProjectForm @added="handleProjectAdded"></AddProjectForm>
-  </Dialog>
 </template>
 <script setup lang="ts">
 import { onMounted, ref } from 'vue'
@@ -13,12 +9,6 @@ import { useProjects } from '@/composables/projects/useProjects'
 import ProjectCard from '@/components/cards/ProjectCard.vue'
 import AddProjectForm from '@/components/forms/AddProjectForm.vue'
 
-const addProjectVisiable = ref<boolean>(false)
-
 const { data: projects, isLoading, error, fetch } = useProjects()
 onMounted(fetch)
-
-const handleProjectAdded = () => {
-  fetch()
-}
 </script>
